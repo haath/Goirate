974cca28098343322dce32586a5e315ea3600a68
Haath <gmantaos@gmail.com>
2018-05-17 22:44:47 +0300

    ci test

--- scanner/main.go
+++ scanner/main.go
@@ -5,7 +5,7 @@ import "github.com/jessevdk/go-flags"
 func main() {
 
 	var opts struct {
-		Verbose bool `short:"v" long:"verbose" description:"Show more information"`
+		Verbose bool `short:"v" long:"verbose" description: "Show more information"`
 	}
 
 	flags.Parse(&opts)
