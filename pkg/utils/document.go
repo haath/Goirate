157400311788cbae65202fbcff3809b9d5379603
Haath <gmantaos@gmail.com>
2018-06-06 22:05:08 +0300

    Fixed HTTPGet wrapper exiting the program on error codes

--- pkg/utils/document.go
+++ pkg/utils/document.go
@@ -9,13 +9,14 @@ import (
 // HTTPGet fetches an HTTP url and returns a goquery.Document
 func HTTPGet(url string) (*goquery.Document, error) {
 	res, err := http.Get(url)
+
 	if err != nil {
-		log.Fatal(err)
 		return nil, err
 	}
+
 	defer res.Body.Close()
+
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 		return nil, err
 	}
 
